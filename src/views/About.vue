329aee0cd0ad977680954c490a40fef1ea21b488
csuwwei <33022015+csuwwei@users.noreply.github.com>
2019-11-12 22:54:20 +0800

    vue前端第一版本，打通了前后端

---
+++ src/views/About.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1>{{returnValue}}</h1>
+  </div>
+</template>
+
+
+<script>
+
+import axios from "axios";
+export default {
+  data(){
+    return{
+      returnValue:''
+    }
+  },
+  methods: {
+      init: function(e) {
+          axios.get("http://localhost:9000/wykd-user/userService/index").then(res => {
+            console.log(res);
+            alert(res.data);
+            this.returnValue = res.data;
+          });
+      }
+  },
+  created:function(){
+    this.init();
+  }
+}
+
+
+</script>
